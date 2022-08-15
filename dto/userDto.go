8e20c2c71c3c19f414ad173faed573723cad6198
viky1sr <vikymuhamad@gmail.com>
2022-08-16 02:32:21 +0700

    fuix dto

--- dto/userDto.go
+++ dto/userDto.go
@@ -7,7 +7,7 @@ type UserCreateDTO struct {
 }
 
 type UserUpdateDTO struct {
-	ID       uint64 `json:"id" form:"id" binding:"required"`
+	ID       uint64 `json:"id" form:"id"`
 	Name     string `json:"name" form:"name" binding:"required"`
 	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
 	Password string `json:"password,omitempty" form:"password,omitempty" validate:"min:6,max:12"`
